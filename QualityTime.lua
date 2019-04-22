d20747bf4a612ac54598b02f8264f16edac51549
David Delamater <David.W.Delamater@gmail.com>
2019-04-21 20:50:26 -0500

    Got something to work again

--- QualityTime.lua
+++ QualityTime.lua
@@ -1,37 +1,36 @@
+local in_combat = false
+local refresh_rate_hz = 5
+local time_till_refresh = (1 / refresh_rate_hz)
 
 --[[ Create the main frame ]]--
-local main_frame = CreateFrame("Frame", nil, UIParent)
-main_frame:SetScript("OnLoad", InitializeMainFrame)
-main_frame:SetScript("OnEvent", MainFrameEventHandler)
+local main_frame = CreateFrame("Frame", "MainFrame", UIParent)
+main_frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+main_frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+main_frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+main_frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 
-local function InitializeMainFrame()
-    print("Howdy")
-    main_frame:RegisterEvent("ADDON_LOADED")
-    main_frame:RegisterEvent("PLAYER_REGEN_ENABLED")
-	main_frame:RegisterEvent("PLAYER_REGEN_DISABLED")
-    main_frame:RegisterEvent("PLAYER_TARGET_CHANGED")
-    main_frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-    main_frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
-    main_frame:SetWidth(100)
-    main_frame:SetHeight(100)
-    main_frame:SetPoint("CENTER", 0, 0)
-    main_frame:SetBackdrop({
-        bgFile = "Interface/Tooltips/UI-Tooltip-Background",
-        edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
-        tile = true,
-        tileSize = 16,
-        edgeSize = 16,
-        insets = { left = 4, right = 4, top = 4, bottom = 4}
-    })
-    main_frame:SetBackdropColor(0,0,0,1)
-    main_frame.text = main_frame:CreateFontString(nil, "ARTWORK")
-    main_frame.text:SetFont("Fonts\\ARIALN.ttf", 14, "OUTLINE")
-    main_frame.text:SetText("Howdy!")
-    main_frame.text:SetPoint("TOPLEFT",10,-10)
-    main_frame:Show()
+local function MainFrame_Initialize(self)
+    print("Loaded")
 end
 
-local function MainFrameEventHandler(event, ...)
-    print(event)
+local function MainFrame_OnEvent(self, event, ...)
+    if (event == "PLAYER_ENTERING_WORLD") then
+        MainFrame_Initialize()
+    elseif (event == "PLAYER_REGEN_ENABLED") then
+        in_combat = false
+    elseif (event == "PLAYER_REGEN_DISABLED") then
+        in_combat = true
+    elseif (event == "COMBAT_LOG_EVENT_UNFILTERED") then
+    end
 end
 
+local function MainFrame_OnUpdate(self, elapsed)
+    time_till_refresh = time_till_refresh - elapsed
+    if (time_till_refresh < 0 ) then
+    else
+        
+    end
+end
+
+main_frame:SetScript("OnEvent", MainFrame_OnEvent)
+main_frame:SetScript("OnUpdate", MainFrame_OnUpdate)
